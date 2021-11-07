7f2a15636bf5bbc979cb1f3f1aff0091c70366b0
BURAK ÖĞRETİCİ <burakogreticii@gmail.com>
2021-11-07 08:42:00 +0300

    08:41 => friendNumbers

--- src/findPrime.js
+++ src/findPrime.js
@@ -1,19 +1,18 @@
 //Question1
 function findPrime(...numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-      let counter = 0;
-      for (let j = 2; j < numbers[i]; j++) {
-        if (numbers[i] % j == 0) {
-          counter++;
-          break; 
-        }
-      }
-      if (counter == 0) {
-        console.log(numbers[i] + " is a prime number")
-      } else {
-        console.log(numbers[i] + " not prime!")
+  for (let i = 0; i < numbers.length; i++) {
+    let counter = 0;
+    for (let j = 2; j < numbers[i]; j++) {
+      if (numbers[i] % j == 0) {
+        counter++;
+        break;
       }
     }
+    if (counter == 0) {
+      console.log(numbers[i] + " is a prime number")
+    } else {
+      console.log(numbers[i] + " not prime!")
+    }
   }
-  findPrime(6, 128, 2, 45, 23, 5, 11, 15)
-  
+}
+findPrime(6, 128, 2, 45, 23, 5, 11, 15)
